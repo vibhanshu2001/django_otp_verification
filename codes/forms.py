1f23473b32796170c09e2f3cf193d4e2c4a6436b
Vibhanshu Garg <v2001.garg@gmail.com>
2021-04-25 23:19:42 +0530

    Twilio

---
+++ codes/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Code
+class CodeForm(forms.ModelForm):
+    number = forms.CharField(label='Code',help_text='Enter SMS verification code')
+    class Meta:
+        model = Code
+        fields = ('number',)
