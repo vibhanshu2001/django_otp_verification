840b8aa575352917ecc78a7fb65ef9b728d91f08
Vibhanshu Garg <v2001.garg@gmail.com>
2021-04-25 23:43:05 +0530

    Twilio

--- otpproj/utils.py
+++ otpproj/utils.py
@@ -1,12 +1,12 @@
 import os
 from twilio.rest import Client
-account_sid = 'AC29ff7a8f1b129190d5c22baaf922056c'
-auth_token = 'a135efbd53ade5a4a28bd57e75a75d03'
+account_sid = 'your sid here'
+auth_token = 'your token here'
 client = Client(account_sid, auth_token)
 def send_sms(user_code,phone_number):
     message = client.messages.create(
         body= f'Hi! You user and verification code is {user_code}',
-        from_ = '+15205829254',
+        from_ = 'your twilio phone number',
         to = f'{phone_number}'
 
     )
